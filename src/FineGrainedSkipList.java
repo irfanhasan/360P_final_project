bf64e23cacf7986834e539bfcf612ab989f31cb9
irfanhasan <ihasan03@gmail.com>
2017-04-18 15:37:30 -0500

    Added SkipList node into FineGrained

--- src/FineGrainedSkipList.java
+++ src/FineGrainedSkipList.java
@@ -2,8 +2,28 @@
  * Implementation of a concurrent SkipList using a fine-grained approach.
  */
 
+import java.util.concurrent.Semaphore;
+import java.util.ArrayList;
+
 public class FineGrainedSkipList<T extends Comparable<T>> implements SkipList<T> {
     
+    private static final int maxHeight = 32;
+    
+    private class SkipListNode<T> {
+        T value;
+        Semaphore lock;
+        ArrayList<SkipListNode<T>> nexts;
+        boolean marked;
+        boolean fullyLinked;
+
+        SkipListNode(T val) {
+            value = val;
+            lock = new Semaphore(1);
+            nexts = new ArrayList<SkipListNode<T>>(maxHeight);
+            marked = false;
+            fullyLinked = false;
+        }
+    }
     /**
      * Public functions
      */
