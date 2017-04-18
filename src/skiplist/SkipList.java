eb59441cb101f469afe4436052ce4c51e30411f9
Gunalan Karun <gunalan.karun@gmail.com>
2017-04-18 02:01:53 -0500

    Implemented add method

--- src/skiplist/SkipList.java
+++ src/skiplist/SkipList.java
@@ -35,7 +35,60 @@ public class SkipList<E> {
 	}
 	
 	public boolean add(E value) {
-
+		if (contains(value)) { return false; }
+		SkipListNode<E> current = head;
+		// Find node before insertion
+		while (current != null) {
+			if ((current.getValue() == null || current.getValue() < value) && (current.right == null || current.right.getValue() > value) && current.down == null) {
+				return current;
+			}
+			
+			if (current.right == null || current.right.getValue() > current.getValue()) {
+				current = current.down;
+			} else {
+				current = current.right;
+			}
+		}
+		
+		// Calculate height
+		int count = 1;
+		while (true) {
+			int r = ThreadLocalRandom.current().nextInt(0, 2);
+			if (r == 1) {
+				count++;
+			} else { 
+				break; 
+			}
+		}
+		
+		ArrayList<SkipListNode<E>> addedNodes = new ArrayList<SkipListNode<E>>();
+		for (int i = 0; i < count; i++) {
+			// Insert in row (modify right and left nodes)
+			next = current.right;
+			newNode = SkipListNode<E>(value);
+			newNode.left = current;
+			newNode.right = next;
+			current.right = newNode;
+			if (next != null) {
+				next.left = newNode;
+			}
+			
+			// Add up and down nodes
+			if (i > 0) {
+				newNode.down = addedNodes.get(i-1);
+				addedNodes.get(i-1).up = newNode;
+			}
+			
+			addedNodes.add(newNode);
+			
+			while (current.up == null) {
+				current = current.left;
+			}
+			current = current.up;
+			
+		}
+		
+		return true;
 	}
 	
 	public boolean remove(E value) {
