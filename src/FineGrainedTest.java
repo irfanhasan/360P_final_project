edc32579f92c33e4a4348b472bf1d26d54c3fb77
irfanhasan <ihasan03@gmail.com>
2017-04-21 01:31:21 -0500

    added remove test case in our simple test

--- src/FineGrainedTest.java
+++ src/FineGrainedTest.java
@@ -9,8 +9,17 @@ public class FineGrainedTest {
 		
 		for(int i=0; i<10; i++){
 			if(mList.contains(i)){
-				System.out.println("Adding " + i + "successful");
+				System.out.println("Adding " + i + " successful");
 			}
 		}
+
+        for (int i = 0; i < 10; i++) {
+            mList.remove(i);
+            if (mList.contains(i)) {
+                System.out.println("oops fail");
+            } else {
+                System.out.println(i + " succesfully removed");
+            }
+        }
 	}
 }
